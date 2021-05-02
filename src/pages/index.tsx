ff949d77e60f77704c5097b24d99a9487449b4b1
unknown <yuto1013@gmail.com>
2021-05-02 23:07:23 +0900

    アイテムを削除する関数を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,22 @@ const Home = () => {
   const add = () => {
     setItems((items) => items.concat(['Hell, word!']));
   };
+  const remove = (index: number) => {
+    setItems((items) => {
+      return items.splice(index, 1);
+    });
+  };
   return (
     <>
       <button onClick={add}>Add Item</button>
       <div>
         {items.map((item) => {
-          return <li>{item}</li>;
+          return (
+            <>
+              <li key={item}>{item}</li>
+              <button onClick={undefined}>Delete</button>
+            </>
+          );
         })}
       </div>
     </>
