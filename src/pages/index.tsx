6222e6d9d54f430a96560585dfbcb842c1276a6e
takeyu1013 <yuto1013@gmail.com>
2021-05-05 19:34:15 +0900

    背景色を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,20 +23,20 @@ const Home = () => {
     setNewItem(() => event.target.value);
   };
   return (
-    <>
+    <div className="min-h-screen bg-gray-100">
       <input className="px-2 py-1" value={newItem} onChange={change} />
       <button className="px-2 py-1 rounded text-white bg-green-500 hover:bg-green-700" onClick={add}>Add Item</button>
       <div>
         {items.map((item, index) => {
           return (
-            <>
+            <div className="bg-white">
               <li key={index}>{item}</li>
               <button onClick={remove.bind(this, index)}>Delete</button>
-            </>
+            </div>
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
 
