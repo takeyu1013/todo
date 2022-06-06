eb45ab7f0abdb543f61df7f8567409faa3a3662a
takeyu1013 <yuto1013@gmail.com>
2022-06-06 22:13:09 +0900

    🎉

--- src/lib/supabase.ts
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_KEY as string);
