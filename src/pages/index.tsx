3234f9b122e5b6bef83efb02ccdfd27dd5fc955c
takeyu1013 <yuto1013@gmail.com>
2022-06-07 22:11:56 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Avatar } from "@mantine/core";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <header>
-      <h1 className="mt-7 ml-48 mb-0 text-3xl font-bold text-rose-600">Todo</h1>
+    <header className="flex px-48 py-6">
+      <h1 className="my-0 text-3xl font-bold text-rose-600">Todo</h1>
+      <Avatar className="ml-auto" />
     </header>
   );
 };
