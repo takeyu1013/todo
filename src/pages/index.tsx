b5ea96dd1e2f32b36e353859a1d9868a77c0977c
takeyu1013 <yuto1013@gmail.com>
2022-06-08 21:33:45 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
-import { Avatar } from "@mantine/core";
+import { Avatar, Menu } from "@mantine/core";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <header className="flex px-48 py-6">
-      <h1 className="my-0 text-3xl font-bold text-rose-600">Todo</h1>
-      <Avatar className="ml-auto" />
-    </header>
+    <div>
+      <header className="flex px-48 py-6">
+        <h1 className="my-0 text-3xl font-bold text-rose-600">Todo</h1>
+        <Menu className="ml-auto" control={<Avatar />}>
+          <Menu.Item>設定</Menu.Item>
+          <Menu.Item>ログアウト</Menu.Item>
+        </Menu>
+      </header>
+      <main className="mx-20">
+        <div className="mx-6 mt-6">
+          <h2 className="my-0">今日する</h2>
+        </div>
+      </main>
+    </div>
   );
 };
 
