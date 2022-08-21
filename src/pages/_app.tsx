63792125cb0c27ad6e3038c44516d96374b09a60
takeyu1013 <yuto1013@gmail.com>
2022-08-21 22:29:42 +0900

    🛂

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { createEmotionCache, MantineProvider } from "@mantine/core";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,7 +16,12 @@ const App = ({ Component, pageProps }: AppProps) => {
       emotionCache={createEmotionCache({ key: "mantine" })}
     >
       <ClerkProvider {...pageProps}>
-        <Component {...pageProps} />
+        <SignedIn>
+          <Component {...pageProps} />
+        </SignedIn>
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
       </ClerkProvider>
     </MantineProvider>
   );
