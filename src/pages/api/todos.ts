60a8185ddd91c93a3d27bdd44a791647d9f2649b
takeyu1013 <yuto1013@gmail.com>
2023-01-05 14:48:25 +0900

    🚧

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -1,22 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { RequireAuthProp } from "@clerk/nextjs/api";
 
 import { prisma } from "src/lib/prisma";
+import { requireAuth, users } from "@clerk/nextjs/api";
 
-const handler = async (
-  { method, body: text }: NextApiRequest,
-  { status }: NextApiResponse
-) => {
-  if (method === "GET") {
-    return status(200).json(await prisma.todo.findMany());
-  } else if (method === "POST") {
-    return status(201).json(
-      await prisma.todo.create({
-        data: {
-          text,
-        },
-      })
-    );
+const handler = requireAuth(
+  async (
+    { method, body: text, auth: { userId } }: RequireAuthProp<NextApiRequest>,
+    { status }: NextApiResponse
+  ) => {
+    const user = await users.getUser(userId);
+    const primaryEmailAddress = user.emailAddresses.find(
+      (emailAddress) => emailAddress.id === user.primaryEmailAddressId
+    )?.emailAddress;
+    console.log("email:", primaryEmailAddress);
+
+    if (method === "GET") {
+      return status(200).json(await prisma.todo.findMany());
+    } else if (method === "POST") {
+      return status(201).json(
+        await prisma.todo.create({
+          data: {
+            text,
+          },
+        })
+      );
+    }
   }
-};
+);
 
 export default handler;
