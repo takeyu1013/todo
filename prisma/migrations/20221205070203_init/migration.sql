26ff11e5df832dcf8964ad93b83274d2e93ad93f
takeyu1013 <yuto1013@gmail.com>
2023-01-07 11:21:55 +0900

    💚

--- prisma/migrations/20221205070203_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "text" TEXT NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
-);
